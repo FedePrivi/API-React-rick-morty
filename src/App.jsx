7c9dc8552906350b67fd2173c6f9b9a8e87faa36
Federico Privitera <fedeprivi2001@gmail.com>
2022-06-23 00:02:16 -0300

    Boton reset de busqueda

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ const App = () => {
   const [nombrePersonaje, setNombrePersonaje] = useState('')
 
 
+  const resetearBusqueda = () => {
+    setNombrePersonaje("")
+  }
+
   //////////////////////////
   //LOCALSTORAGE
   /////////////////////////
@@ -19,19 +23,20 @@ const App = () => {
 
   useEffect(() => {
     localStorage.setItem("nombreApi", JSON.stringify(nombrePersonaje))
-    console.log(nombrePersonaje);
+    // console.log(nombrePersonaje);
   }, [nombrePersonaje])
   
   //////////////////////////
   //LOCALSTORAGE
   /////////////////////////
 
-  
+
   return (
     <div className='container'>
 
         <h1>APP-Rick and Morty</h1>
         <Formulario setNombrePersonaje={setNombrePersonaje}/>
+          <button type="button" className="btn btn-success mt-2" onClick={resetearBusqueda}>Reset</button>
         <PintarDatos nombrePersonaje={nombrePersonaje}/>
         
 
